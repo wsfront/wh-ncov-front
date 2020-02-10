5ccba1f051ec6b676c967edbd37aa6023be89d2c
e-Vicky.Liu <e-vicky.liu@homecredit.cn>
2020-02-10 23:43:04 +0800

    add:componentcheckindex

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #FAFAFA;
   background: #fafafa;
   /*margin-top: 60px;*/
 }
