136a7017121b3b0790afdd8efc6bbac4ea9b994f
lkaien <lkaientan@gmail.com>
2020-02-12 23:03:05 +0800

    merge 冲突处理

--- src/components/EndHospitalEdit.vue
+++ src/components/EndHospitalEdit.vue
@@ -303,7 +303,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
   .el-row {
     margin-bottom: 10px;
     &:last-child {
