177fdc95e53202005cbcebf05e97025df330ef5f
e-Vicky.Liu <e-vicky.liu@homecredit.cn>
2020-02-17 23:44:55 +0800

    hospital detail

--- src/components/EditHospital.vue
+++ src/components/EditHospital.vue
@@ -1,33 +1,47 @@
 <template>
-  <el-dialog
-    :visible.sync="info"
-    width="90%"
-    center
-    v-if="info.name"
-    :title="info.name"
-  >
+  <div class="info-wrapper">
     <el-form
       label-position="left"
-      label-width="200px"
-      :model="info"
+      label-width="100px"
       size="small"
     >
-      <el-form-item label="名称">
+      <el-form-item  v-for="item in info" :key="item.key" :label="item.name" class="form-title">
+
+        <el-switch
+        v-if="item.type==='switch'"
+        v-model="item.value"
+        :disabled="readonly"
+        active-color="#13ce66"
+        inactive-color="#ff4949">
+        </el-switch>
+
         <el-input
-          v-model="info.receive_accouche"
+         v-else-if="item.type==='textarea'"
+         type="textarea"
+         :disabled="readonly"
+         :autosize="{ minRows: 2, maxRows: 8}"
+        v-model="item.value">
+        </el-input>
+
+        <div v-else-if="item.type==='special'"
+           v-for="phone in item.value"
+           :key="phone.id">
+          <el-input
+          :disabled="readonly"
+          v-model="phone.department">
+          </el-input>
+          <el-input
+          :disabled="readonly"
+          v-model="phone.phone">
+          </el-input>
+        </div>
+
+        <el-input
+          v-else
+          v-model="item.value"
           :disabled="readonly"
         ></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-input v-model="info.receive_check" :disabled="readonly"></el-input>
-      </el-form-item>
-      <el-form-item label="中孕期三维排畸彩超（大排畸）">
-        <el-input
-          v-model="info.receive_clour_ultrasound"
-          :disabled="readonly"
-          :maxlength="5"
-          width="20"
-        ></el-input>
+
       </el-form-item>
     </el-form>
     <div>
@@ -40,15 +54,94 @@
         <el-button @click="goSave">保存</el-button>
       </div>
     </div>
-  </el-dialog>
+  </div>
 </template>
 <script>
 export default {
-  props: ["current"],
   data() {
     //   console.log('ddd',this)
     return {
-      info: "",
+      info: [
+        {
+          type: "switch",
+          key: "verify",
+          name: "已核实",
+          value: ''
+        },
+        {
+          type: "accept",
+          key: "receive_normal",
+          name: "普通孕妇",
+          value: ''
+        },
+        {
+          type: "accept",
+          key: "receive_sick",
+          name: "疑似/确诊孕妇",
+          value: ''
+        },
+        {
+          type: "check",
+          key: "receive_normal_check",
+          name: "常规产检",
+          value: ''
+        },
+        {
+          type: "check",
+          key: "receive_ultrasound",
+          name: "孕期产检B超",
+          value: ''
+        },
+        {
+          type: "check",
+          key: "receive_clour_ultrasound",
+          name: "中孕期三维排畸彩超（大排畸）",
+          value: ''
+        },
+        {
+          type: "check",
+          key: "receive_accouche",
+          name: "接生",
+          value: ''
+        },
+        {
+          type: "other",
+          key: "receive_check",
+          name: "核酸检测",
+          value: ''
+        },
+        {
+          type: "textarea",
+          key: "remark",
+          name: "补充说明",
+          value: ''
+        },
+        {
+          type: "base",
+          key: "area",
+          name: "地区",
+          value: ''
+        },
+        {
+          type: "textarea",
+          key: "address",
+          name: "地址",
+          value: ''
+        },
+
+        {
+          type: "special",
+          key: "phones",
+          name: "电话",
+          value: [
+            {
+              department: "院办",
+              id: 1,
+              phone: "027-84918001"
+            }
+          ]
+        }
+      ],
       readonly: true
     };
   },
@@ -57,21 +150,24 @@ export default {
       this.$emit("back");
     },
     goEdit() {
+      console.log(this.info)
       this.readonly = false;
     },
     goCancel() {
       Object.assign(this.info, this.current);
       this.readonly = true;
     },
-    goSave() {
-      
-    }
-  },
-  watch: {
-    current(v) {
-      this.info = Object.assign({}, v);
+    goSave() { },
+    updateData(v) {
+      this.info.forEach(item => {
+        item.value = v[item.key]
+      })
     }
   }
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.info-wrapper /deep/.el-form-item--small{
+  padding:0 10px;
+}
+</style>
