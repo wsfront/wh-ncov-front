bd615645738e208dd7f4a3e7fb255379b964b5cf
lkaien <lkaientan@gmail.com>
2020-03-21 18:27:36 +0800

    remove redundancy

--- src/components/EaseHandbook.vue
+++ src/components/EaseHandbook.vue
@@ -3,10 +3,10 @@
     <div class="hearder-block">
       <HeaderLayout :activeIndex="2" />
     </div>
-    <v-touch v-on:swipeup="handleNav()">
+    <v-touch v-on:swipeup="handleNav">
       <div class="launch-main">
         <img
-          @click="handleNav()"
+          @click="handleNav"
           class="launch-btn"
           src="http://wuhan2099.oss-accelerate.aliyuncs.com/ease_button.png"
         />
@@ -66,175 +66,6 @@ export default {
   color: #333;
 }
 
-.jump-link {
-  position: absolute;
-  height: 22px;
-  width: 20%;
-  bottom: 10%;
-  right: 10%;
-  // background: antiquewhite;
-  opacity: 0.8;
-  &._0101 {
-    width: 42%;
-    bottom: 20%;
-    right: 30%;
-  }
-  &._03 {
-    width: 31%;
-    bottom: 10%;
-    right: 16%;
-  }
-  &._06 {
-    width: 18%;
-    bottom: 70%;
-    right: 16%;
-  }
-  &._0602 {
-    bottom: 5%;
-    right: 38%;
-  }
-  &._0108 {
-    bottom: 9%;
-    right: 23%;
-  }
-  &._0109 {
-    width: 45%;
-    bottom: 59%;
-    right: 31%;
-    &._2 {
-      bottom: 45%;
-    }
-    &._3 {
-      bottom: 30%;
-    }
-  }
-  &._0208 {
-    bottom: 53%;
-    right: 57%;
-    &._2 {
-      bottom: 36%;
-    }
-  }
-  &._0311 {
-    &._1 {
-      width: 45%;
-      bottom: 67%;
-      right: 30%;
-    }
-    &._2 {
-      width: 22%;
-      bottom: 55%;
-      right: 54%;
-    }
-    &._3 {
-      width: 33%;
-      bottom: 43%;
-      right: 42%;
-    }
-    &._4 {
-      width: 33%;
-      bottom: 24%;
-      right: 42%;
-    }
-  }
-  &._4101 {
-    width: 33%;
-    bottom: 8%;
-    right: 38%;
-  }
-  &._4102 {
-    width: 20%;
-    bottom: 20%;
-    right: 52%;
-    &._2 {
-      width: 13%;
-      bottom: 20%;
-      right: 38%;
-    }
-  }
-  &._4103 {
-    width: 32%;
-    bottom: 30%;
-    right: 38%;
-  }
-  &._4108 {
-    width: 37%;
-    bottom: 32%;
-    right: 38%;
-  }
-  &._4206 {
-    bottom: 65%;
-    right: 46%;
-  }
-  &._4207 {
-    width: 47%;
-    bottom: 31%;
-    right: 29%;
-  }
-  &._0504 {
-    right: 57%;
-    &._1 {
-      bottom: 60%;
-    }
-    &._2 {
-      bottom: 45%;
-    }
-    &._3 {
-      bottom: 30%;
-    }
-  }
-  &._0604 {
-    width: 35%;
-    right: 40%;
-    &._1 {
-      bottom: 56%;
-    }
-    &._2 {
-      bottom: 36%;
-    }
-  }
-  &._0701 {
-    width: 33%;
-    bottom: 3%;
-    right: 38%;
-  }
-  &._0704 {
-    width: 35%;
-    bottom: 54%;
-    right: 41%;
-    &._2 {
-      bottom: 37%;
-    }
-  }
-  &._0814 {
-    &._1 {
-      width: 19%;
-      bottom: 54%;
-      right: 58%;
-    }
-    &._2 {
-      width: 35%;
-      bottom: 35%;
-      right: 42%;
-    }
-  }
-  &._0901 {
-    &._1 {
-      width: 67%;
-      bottom: 7%;
-      right: 16%;
-    }
-    &._2 {
-      width: 54%;
-      bottom: 4%;
-      right: 29%;
-    }
-  }
-  &._0903 {
-    bottom: 18%;
-    right: 19%;
-  }
-}
 .hearder-block {
   margin: 10px 0 0;
 }
@@ -269,52 +100,6 @@ export default {
     margin-top: 3vh;
   }
 }
-
-// @media screen and (max-width: 350px){
-//   .catalog{
-//     font-size: 13px;
-//     &-header {
-//       font-size: 15px;
-//     }
-//   }
-// }
-// @media screen and (min-width: 400px){
-//   .catalog{
-//     font-size: 15px;
-//     &-header {
-//       font-size: 17px;
-//     }
-//   }
-// }
-// .booklet {
-//   // position: relative;
-//   height: 100%;
-//   -webkit-overflow-scrolling : touch;
-//   overflow-y: scroll;
-//   &-img {
-//     width: 100%;
-//     display: block;
-//   }
-//   &-item {
-//     position: relative;
-//   }
-// }
-// .goto {
-//   width: 26.5%;
-//   height: 5%;
-//   position: absolute;
-//   top: 38.7%;
-//   left: 37.5%;
-//   display: block;
-// }
-// .get-more {
-//   width: 32.2%;
-//   height: 4.5%;
-//   position: absolute;
-//   right: 13.5%;
-//   bottom: 17%;
-//   display: block;
-// }
 img[lazy="loading"] {
   width: 32px;
   margin: 0 auto;
